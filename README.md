7a7d035d9e814db655d29aa9e7e1c2cb61a99c02
gdifiore <itzg3bz@gmail.com>
2018-03-14 21:55:46 -0400

    quick_info.py

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # xkcd
 xkcd - tools for xkcd comics
+
+    quick_info.py
+Grab's title and alt for the latest xkcd comic. Example: <br>
+<br>
+`Violin Plots:  Strictly speaking, 'violin' refers to the internal structure of the data. The external portion visible in the plot is called the 'viola.'`
---
+++ src/quick_info.py
@@ -0,0 +1,14 @@
+#
+# quick_info.py - xkcd
+#
+# (c) gdifiore 2018
+#
+
+import urllib.request
+import json
+
+url = "https://xkcd.com/info.0.json" # url is link to json to latest xkcd
+response = urllib.request.urlopen(url)
+data = json.loads(response.read()) # load and parse json
+
+print(data['title'] + ": " ,data['alt']) # grab title + alt data form parsed json
