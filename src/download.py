200819af179d73b3a92f9d0f99325d4fb94f919d
gdifiore <itzg3bz@gmail.com>
2018-03-15 17:10:56 -0400

    download.py

---
+++ src/download.py
@@ -0,0 +1,22 @@
+#
+# download.py - xkcd
+#
+# (c) gdifiore 2018
+#
+
+import urllib.request
+import json
+
+url = "https://xkcd.com/info.0.json" # url is link to json to latest xkcd
+response = urllib.request.urlopen(url)
+data = json.loads(response.read()) # load and parse json
+
+num = data['num']
+img_url = data['img'] # grab img link form parsed json
+
+
+print(img_url)
+
+urllib.request.urlretrieve (img_url, "xkcd_latest.png")
+print()
+print("Downloaded xkcd number", num)
