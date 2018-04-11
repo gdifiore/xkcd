1e8e3db83a10fd6aa3fdc84ae1cd2514fc379f9a
Gabe <itzg3bz@gmail.com>
2018-04-11 10:18:08 -0400

    Commenting consistency
    Desktop GitHub on mobile yay

--- src/download.py
+++ src/download.py
@@ -11,7 +11,7 @@ url = "https://xkcd.com/info.0.json" # url is link to json to latest xkcd
 response = urllib.request.urlopen(url) # open url with urllib.request
 data = json.loads(response.read()) # load and parse json
 
-num = data['num'] # grab xkcd number from json
+num = data['num'] # grab xkcd number from parsed json
 img_url = data['img'] # grab img link form parsed json
 
 
