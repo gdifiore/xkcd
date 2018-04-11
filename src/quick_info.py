285fbc47ae8c425c1f83ab385cd54fe593d5dd5b
Gabe <itzg3bz@gmail.com>
2018-04-11 10:19:51 -0400

    update

--- src/quick_info.py
+++ src/quick_info.py
@@ -11,7 +11,7 @@ url = "https://xkcd.com/info.0.json" # url is link to json of latest xkcd
 response = urllib.request.urlopen(url) # open url with urllib.request
 data = json.loads(response.read()) # load and parse json
 
-title = data['title'] 
-alt = data['alt']
+title = data['title'] # grab title from parsed json
+alt = data['alt'] # grab alt text from parsed json
 
 print(title + ": " + alt) # grab title + alt data from parsed json
