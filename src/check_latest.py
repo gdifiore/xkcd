008183a504ae6b5306b2482e68df976234ab6a47
gdifiore <itzg3bz@gmail.com>
2018-04-13 15:35:02 -0400

    check_latest

---
+++ src/check_latest.py
@@ -0,0 +1,17 @@
+#
+# check_latest.py - xkcd
+#
+# (c) gdifiore 2018 <difioregabe@gmail.com>
+#
+
+import urllib.request
+import json
+
+url = "https://xkcd.com/info.0.json" # url is link to json to latest xkcd
+response = urllib.request.urlopen(url) # open url with urllib.request
+data = json.loads(response.read()) # load and parse json
+
+num = data['num'] # grab xkcd number from parsed json
+title = data['title'] # grab title from parsed json
+
+print("Latest xkcd is number: " + str(num) + ", " + title) # grab title + alt data from parsed json
